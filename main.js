37a2c9dc1a74c6466e53e57f485316de99de39ab
Subhan <159427520+Subhantagiyev@users.noreply.github.com>
2024-07-01 23:41:36 +0400

    0dan/js

---
+++ main.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = a++;
+document.write("a="+a);
+document.write("<br>");
+document.write("b="+b);
+
+// eger a++ yazdiqda a-nin deyeri 1 vahid artir amma evvelki deyeri qaytarirlir
+//eger ++a yazdiqda  a-nin deyeri 1 vahid artir hem de artirdigi qiymeti qaytarir
