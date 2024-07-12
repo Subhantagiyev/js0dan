cee240d39b8b0b4b34e906df12a548e98e3fb1ed
Subhan <159427520+Subhantagiyev@users.noreply.github.com>
2024-07-12 23:31:02 +0400

    0dan/js

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-let a = 6;
-let b = a++;
-document.write("a="+a);
-document.write("<br>");
-document.write("b="+b);
+//let a = 6;
+//let b = a++;
+//document.write("a="+a);
+//document.write("<br>");
+//document.write("b="+b);
 
 // eger a++ yazdiqda a-nin deyeri 1 vahid artir amma evvelki deyeri qaytarirlir
 //eger ++a yazdiqda  a-nin deyeri 1 vahid artir hem de artirdigi qiymeti qaytarir
+
+
+  // IF VE ELSE 
+
+  let bal = 65 ;
+  if(bal>50){
+    console.group("qebul oldunuz:" + bal);
+  }else{
+    console.log("kesildiniz:" + bal);
+  }
